192e10a49ca1e14760197506e0224223a6515a28
garima-ship-it <garima@kashware.com>
2026-04-15 11:49:30 +0530

    Update RewardsSection.tsx

--- src/components/home/RewardsSection.tsx
+++ src/components/home/RewardsSection.tsx
@@ -1,18 +1,25 @@
+import Link from 'next/link'
 import { ASSETS } from '@/lib/data'
 
 export default function RewardsSection() {
   return (
     <>
-      <section className="rewards-desktop" style={{ position: 'relative', width: '100%', height: 693, overflow: 'hidden' }}>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src={ASSETS.rewardsBg} alt="Most Rewarding Payment Card"
-          style={{ position: 'absolute', width: '100%', height: '122.94%', top: '-16.74%', left: 0, objectFit: 'cover' }} />
+      {/* ── DESKTOP ── */}
+      <section className="rewards-desktop" style={{ position: 'relative', width: '100%', overflow: 'hidden' }}>
+        <Link href="/collection" style={{ display: 'block' }}>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src={ASSETS.rewardsBg} alt="Most Rewarding Payment Card"
+            style={{ width: '100%', height: 'auto', display: 'block' }} />
+        </Link>
       </section>
 
+      {/* ── MOBILE ── */}
       <section className="rewards-mobile" style={{ display: 'none', background: '#080808', width: '100%' }}>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src={ASSETS.rewardsBgMobile} alt="Most Rewarding Payment Card"
-          style={{ width: '100%', height: 'auto', display: 'block', objectFit: 'cover' }} />
+        <Link href="/collection" style={{ display: 'block' }}>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src={ASSETS.rewardsBgMobile} alt="Most Rewarding Payment Card"
+            style={{ width: '100%', height: 'auto', display: 'block' }} />
+        </Link>
       </section>
 
       <style>{`
